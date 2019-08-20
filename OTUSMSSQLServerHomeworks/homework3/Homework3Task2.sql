1c2843e21d17e6a11ce8e8b60e78633964a53443
Alexey Knyazev <joalexey@gmail.com>
2019-08-20 13:14:15 +0300

    fixes after review

--- OTUSMSSQLServerHomeworks/homework3/Homework3Task2.sql
+++ OTUSMSSQLServerHomeworks/homework3/Homework3Task2.sql
@@ -3,7 +3,7 @@ CREATE PROCEDURE [dbo].[Homework3Task2Option1]
 AS
 BEGIN
 	SELECT [StockItemId], [StockItemName], [UnitPrice]
-	FROM [WideWorldImporters].[Warehouse].[StockItems]
+	FROM [Warehouse].[StockItems]
 	WHERE [UnitPrice] <= ALL (SELECT [UnitPrice] FROM [Warehouse].[StockItems])
 END
 
@@ -14,6 +14,6 @@ CREATE PROCEDURE [dbo].[Homework3Task2Option2]
 AS
 BEGIN
 	SELECT [StockItemId], [StockItemName], [UnitPrice]
-	FROM [WideWorldImporters].[Warehouse].[StockItems]
-	WHERE [UnitPrice] = (SELECT MIN(UnitPrice) FROM [WideWorldImporters].[Warehouse].[StockItems])
+	FROM [Warehouse].[StockItems]
+	WHERE [UnitPrice] = (SELECT MIN(UnitPrice) FROM [Warehouse].[StockItems])
 END
