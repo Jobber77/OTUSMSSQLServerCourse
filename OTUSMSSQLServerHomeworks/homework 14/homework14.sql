69351eac20bdd6422786eb9debfffc1725798581
Alexey Knyazev <joalexey@gmail.com>
2019-09-24 18:20:35 +0300

    homework done

---
+++ OTUSMSSQLServerHomeworks/homework 14/homework14.sql
@@ -0,0 +1,37 @@
+﻿-- делаю вариант 2. Взял классы отсюда: https://habr.com/ru/post/88396/
+
+--enable CLR
+EXECUTE sp_configure 'show advanced options', 1;
+GO
+
+RECONFIGURE;
+GO
+
+EXECUTE sp_configure 'clr enabled', 1;
+EXECUTE sp_configure 'clr strict security', 0 
+GO
+
+RECONFIGURE;
+GO
+
+--import assembly
+CREATE ASSEMBLY CLRFunctions FROM 'C:\Users\aleksey.knyazev\Documents\GitHub\OTUSMSSQLServerCourse\SqlClrClasses\bin\Debug\SqlClrClasses.dll' WITH PERMISSION_SET = SAFE;
+GO
+
+--create database function
+CREATE FUNCTION [dbo].[SplitStringCLR]
+(
+	@text [NVARCHAR](MAX), 
+	@delimiter [NCHAR](1)
+)
+RETURNS TABLE 
+(
+	[Order] INT,
+	[StringPart] NVARCHAR(MAX)
+)
+AS
+EXTERNAL NAME CLRFunctions.UserDefinedFunctions.SplitString
+
+GO
+--usage example
+SELECT * FROM [dbo].[SplitStringCLR]('a,b,c,d,', ',')
---
+++ SqlClrClasses/UserDefinedFunctions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+
+public class UserDefinedFunctions
+{
+    [SqlFunction(FillRowMethodName = "SplitStringFillRow", TableDefinition = "part NVARCHAR(MAX), ID_ORDER INT")]
+    public static IEnumerator SplitString(SqlString text, char[] delimiter)
+    {
+        if (text.IsNull)
+            yield break;
+
+        int valueIndex = 1;
+
+        foreach (string s in text.Value.Split(delimiter, StringSplitOptions.RemoveEmptyEntries))
+        {
+            yield return new KeyValuePair<int, string>(valueIndex++, s.Trim());
+        }
+    }
+
+    public static void SplitStringFillRow(object oKeyValuePair, out SqlInt32 valueIndex, out SqlString value)
+    {
+        KeyValuePair<int, string> keyValuePair = (KeyValuePair<int, string>)oKeyValuePair;
+
+        valueIndex = keyValuePair.Key;
+        value = keyValuePair.Value;
+    }
+}
